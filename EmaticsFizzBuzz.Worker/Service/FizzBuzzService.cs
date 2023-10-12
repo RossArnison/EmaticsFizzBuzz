96c1dc5d6d46dca964fc60a2bc4e3bd5936cb737
Ross Arnison <arnisonr@gmail.com>
2023-10-12 15:45:16 +0100

    Templated Service for tests

--- EmaticsFizzBuzz.Worker/Service/FizzBuzzService.cs
+++ EmaticsFizzBuzz.Worker/Service/FizzBuzzService.cs
@@ -2,5 +2,33 @@
 
 public class FizzBuzzService : Interface.IFizzBuzzService
 {
-    
+    public Task<IEnumerable<string>> CreateSequenceAsync(int start, int end, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> CreateSequence(int start, int end)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ValidateInput()
+    {
+        throw new NotImplementedException();
+    }
+
+    private IEnumerable<int> CreateRange(int start, int end)
+    {
+        throw new NotImplementedException();
+    }
+
+    private IEnumerable<string> ConvertToFizzBuzzSequence(IEnumerable<int> range)
+    {
+        throw new NotImplementedException();
+    }
+
+    private string GetFizzBuzzValue(int item)
+    {
+        throw new NotImplementedException();
+    }
 }
--- EmaticsFizzBuzz.Worker/Service/Interface/IFizzBuzzService.cs
+++ EmaticsFizzBuzz.Worker/Service/Interface/IFizzBuzzService.cs
@@ -2,5 +2,21 @@
 
 public interface IFizzBuzzService
 {
+    /// <summary>
+    /// Creates a FizzBuzz sequence
+    /// </summary>
+    /// <param name="start">The first item in the sequence</param>
+    /// <param name="end">The last item in the sequence</param>
+    /// <param name="token">Cancellation Token</param>
+    /// <returns>a list of strings containing the fizz buzz sequence</returns>
+    Task<IEnumerable<string>> CreateSequenceAsync(int start, int end, CancellationToken token);
     
+    
+    /// <summary>
+    /// Creates a FizzBuzz sequence
+    /// </summary>
+    /// <param name="start">The first item in the sequence</param>
+    /// <param name="end">The last item in the sequence</param>
+    /// <returns>a list of strings containing the fizz buzz sequence</returns>
+    IEnumerable<string> CreateSequence(int start, int end);
 }
