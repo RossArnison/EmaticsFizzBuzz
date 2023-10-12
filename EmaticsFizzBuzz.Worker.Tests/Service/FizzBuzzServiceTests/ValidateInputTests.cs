c5619ce212ded41cccd7c7398a32a0e85263b892
Ross Arnison <arnisonr@gmail.com>
2023-10-12 15:50:06 +0100

    Added test util TestBase.cs and created empty service test base

---
+++ EmaticsFizzBuzz.Worker.Tests/Service/FizzBuzzServiceTests/ValidateInputTests.cs
@@ -0,0 +1,9 @@
+﻿using NUnit.Framework;
+
+namespace EmaticsFizzBuzz.Worker.Tests.Service.FizzBuzzServiceTests;
+
+[TestFixture]
+internal class ValidateInputTests
+{
+    
+}
--- EmaticsFizzBuzz.Worker.Tests/Usings.cs
+++ EmaticsFizzBuzz.Worker.Tests/Usings.cs
@@ -1,1 +1,2 @@
 global using NUnit.Framework;
+global using EmaticsFizzBuzz.Worker.Tests.TestUtils;
